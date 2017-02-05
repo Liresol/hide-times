6cc441195a64a7329c904bcefef19994f1b61960
Liresol <bugme1210@yahoo.com>
2017-02-04 21:23:43 -0500

    Added Profiles
    The program now uses profiles in order to keep hiding times in new
    sessions. Also changed the README a little.

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 A small add-on for hiding the times when showing the answers on Anki.
 
-Thrown together in ~2 hours, currently only verified to work on Mac on Verion 2.0.
+Thrown together in a few hours, currently only verified to work on Mac on Verion 2.0.
 
 The code for installing on Anki is 
 **857667597**
 which can be used in the Add-ons menu.
+
+Issues and Pull Requests are appreciated.
--- hide-times.py
+++ hide-times.py
@@ -1,15 +1,18 @@
 __addon_name__ = "Hide Button Times"
-__version__ = "1.0"
+__version__ = "1.1"
 
 from aqt import mw, dialogs
 from aqt.utils import showInfo
 from aqt.qt import *
 from aqt import appVersion
 from aqt.utils import showWarning
+
+from anki.hooks import addHook
 import aqt
 
 ht_state_on = False
 ht_menu = None
+ht_profile_loaded = False
 
 def shownButtonTime(self, i):
     if not self.mw.col.conf['estTimes']:
@@ -21,6 +24,9 @@ def hiddenButtonTime(self, i):
     return "<span class=nobold>Press %d</span><br>" %i
 
 def ht_on():
+    if not ht_profile_loaded:
+        showWarning(HT_ERROR_NO_PROFILE)
+        return False
     try:
         global ht_state_on
         ht_state_on = True
@@ -34,6 +40,9 @@ def ht_on():
 
 
 def ht_off():
+    if not ht_profile_loaded:
+        showWarning(HT_ERROR_NO_PROFILE)
+        return False
     try:
         global ht_state_on
         ht_state_on = False
@@ -45,7 +54,11 @@ def ht_off():
         showWarning(HT_ERROR_SWITCH)
         return False
 
-HT_ERROR_SWITCH = """ Switching the times during review failed: This is a really simple program, so someone really skrewed up."""
+HT_ERROR_SWITCH = """ Switching the times during review failed: This is a really
+simple program, so it should be fixed by waiting a bit."""
+HT_ERROR_NO_PROFILE = """ Switching the times during review failed: The profile
+has not yet been loaded. It could be a bug in Anki or it might be fixed by
+waiting a bit."""
 
 def ht_switch():
     is_active_dialog = filter(bool, [x[1] for x in dialogs._dialogs.values()])
@@ -60,8 +73,27 @@ def ht_switch():
         else:
             ht_on()
 
+def ht_save():
+    mw.pm.profile['ht_state_on'] = ht_state_on
+
+def ht_load():
+    global ht_state_on, ht_profile_loaded
+
+    try:
+        ht_state_on = mw.pm.profile['ht_state_on']
+
+    except KeyError: 
+        ht_state_on = False
+
+    ht_profile_loaded = True
+
+    if ht_state_on:
+        ht_on()
+
 def ht_init():
     global ht_menu
+    addHook("profileLoaded",ht_load)
+    addHook("unloadProfile",ht_save)
     ht_menu = QAction("Hide Button Times", mw, checkable=True)
     ht_menu.triggered.connect(ht_switch)
     try:
@@ -69,7 +101,7 @@ def ht_init():
     except AttributeError:
         mw.addon_view_menu = QMenu(_(u"&View"), mw)
         mw.form.menubar.insertMenu(mw.form.menuTools.menuAction(),
-                                                                mw.addon_view_menu)
+                                   mw.addon_view_menu)
         mw.addon_view_menu.addAction(ht_menu)
 
 ht_init()
